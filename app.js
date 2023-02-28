ff511b9325a1997de3134f1959a8bb0d8e2dfdbd
oksi18 <oksanaklymchuk04@gmail.com>
2023-02-28 13:32:05 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path')
+
+// fs.readdir(path.join('BigFolder'), {withFileTypes: true},(err, data)=>{
+//   if (err) throw new Error();
+//   console.log(data);
+// })
+
+
+// fs.mkdir(path.join('BigFolder'), (err) => {
+//     if (err) throw new Error('error')
+// })
+// --folder 1
+// fs.mkdir(path.join('BigFolder', 'folder1'),(err) => {
+//     if (err) throw new Error('error')
+// });
+
+// --folder 2
+// fs.mkdir(path.join('BigFolder', 'folder2'),(err) => {
+//     if (err) throw new Error('error')
+// });
+
+// --folder 3
+// fs.mkdir(path.join('BigFolder', 'folder3'),(err) => {
+//     if (err) throw new Error('error')
+// });
+
+// --folder 4
+// fs.mkdir(path.join('BigFolder', 'folder4'),(err) => {
+//     if (err) throw new Error('error')
+// });
+
+// --folder 5
+// fs.mkdir(path.join('BigFolder', 'folder5'),(err) => {
+//     if (err) throw new Error('error')
+// });
