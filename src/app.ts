019a977c670e551922577ab3502276574baa3326
oksi18 <oksanaklymchuk04@gmail.com>
2023-03-15 21:55:06 +0200

    lesson 4

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import { config } from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 
-import { configs } from "./configs/config";
 import { userRouter } from "./routers/user.router";
 import { IError } from "./types/common.types";
 
@@ -17,18 +16,22 @@ app.use("/users", userRouter);
 
 app.use((err: IError, req: Request, res: Response, next: NextFunction) => {
   console.log(err);
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const status = err.status;
+  try {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const status = err.status;
 
-  return res.status(status).json({
-    message: err.message,
-    status,
-  });
+    return res.status(status).json({
+      message: err.message,
+      status,
+    });
+  } catch (e) {
+    next(e);
+  }
 });
 console.log(process.env.PORT);
 
-app.listen(configs.PORT, () => {
-  mongoose.connect(configs.DB_URL);
-  console.log(`Server has started on PORT ${configs.PORT}`);
+app.listen(process.env.PORT, () => {
+  mongoose.connect(process.env.DB_URL);
+  console.log(`Server has started on PORT ${process.env.PORT}`);
 });
